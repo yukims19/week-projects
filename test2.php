5b42858811af6bcc81ca4014c488760c029a2f61
Yuki Li <yol107@ucsd.edu>
2017-11-02 11:00:47 -0700

    Delete unecessary files

--- test2.php
+++
@@ -1,9 +0,0 @@
-***REMOVED***
-***REMOVED***
-<a href="http://www.tutorialspoint.com/index.htm">Home</a> -
-     <a href="http://www.tutorialspoint.com/ebxml">ebXML</a> -
-     <a href="http://www.tutorialspoint.com/ajax">AJAX</a> -
-     <a href="http://www.tutorialspoint.com/perl">PERL</a> <br />
-
- ***REMOVED***
-    ***REMOVED***
